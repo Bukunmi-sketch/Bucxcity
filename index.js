29abad4bd3e03cbc7c471b43f3797088c3c806cb
Bukunmi Olarinde <58373089+Bukunmi-sketch@users.noreply.github.com>
2022-07-08 10:44:39 -0700

    added js
    added nav js files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const bars=document.querySelector(".bars");
+/*const bars=document.querySelector(".bars");
 const ul=document.querySelector("header ul");
 bars.onclick=()=>{
      ul.style.left="-100%"
@@ -9,3 +9,19 @@ function toggleit(menu){
 }
 
 */
+
+let barButton=document.querySelector('.bars');
+let headerUl=document.querySelector('header ul');
+let closebar=document.querySelector(".closebar");
+
+barButton.onclick=()=>{
+    headerUl.style.left="0";
+    closebar.style.display="block";
+    barButton.style.display="none";
+}
+
+closebar.onclick=()=>{
+    headerUl.style.left="-100%";
+    closebar.style.display="none";
+    barButton.style.display="block";
+}
